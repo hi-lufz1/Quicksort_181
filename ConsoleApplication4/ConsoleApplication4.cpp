887b7bf1a29675ca279e628a1322dd273c978977
hi-lufz1 <latifufauzi@gmail.com>
2023-03-31 11:11:33 +0700

    search for an element less thanor equal to pivot

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -59,6 +59,10 @@ void q_short(int low, int high)
 				cmp_count++;
 			}
 			cmp_count++;
+			//search for an element less thanor equal to pivot
+			while ((arr[j] > pivot) && (j >= low))//langkah 7
+				j--;//langkah 8
+			cmp_count++;
 		}
 
 
