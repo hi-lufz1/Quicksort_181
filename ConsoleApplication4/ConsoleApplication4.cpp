91a9808edbd95bafa0db00486b19fedacd5231d2
hi-lufz1 <latifufauzi@gmail.com>
2023-03-31 10:54:41 +0700

    display input

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -7,10 +7,21 @@ int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of movement
 int n; 
 void input() {
+	while (true)
+	{
+		cout << "masukan panjang element array :";
+		cin >> n;
 
+		if (n <= 20)
+			break;
+		else
+			cout << "\n Maksimum panjnag array adalah 20" << endl;
 
+	}
 
-
+	cout << "\n------------" << endl;
+	cout << "\nEnter array element" << endl;
+	cout << "\n------------" << endl;
 
 
 
