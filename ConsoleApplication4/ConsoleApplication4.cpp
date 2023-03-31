ae7ae5e4056cb2cf514399807ef1e26314720293
hi-lufz1 <latifufauzi@gmail.com>
2023-03-31 10:57:37 +0700

    3

--- ConsoleApplication4/ConsoleApplication4.cpp
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -19,11 +19,14 @@ void input() {
 
 	}
 
-	cout << "\n------------" << endl;
+	cout << "\n--------------------" << endl;
 	cout << "\nEnter array element" << endl;
-	cout << "\n------------" << endl;
+	cout << "\n--------------------" << endl;
 
-
-
-
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
  }
+// swap the element at index x with the element at index y
